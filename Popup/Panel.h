3e59948cd3c87eefaf2eb25e9b78bac36112b39b
Alex Stelea <alex.stelea@gmail.com>
2013-08-07 12:52:29 -0400

    Removed call to openInChrome from Panel.h

--- Popup/Panel.h
+++ Popup/Panel.h
@@ -3,8 +3,7 @@
 @class WebView;
 @interface Panel : NSPanel{
     WebView* webview;
-    NSURLRequest *request;
+    NSURLRequest* request;
 }
 @property (assign) IBOutlet WebView *webview;
--(void)openInChrome;
 @end
